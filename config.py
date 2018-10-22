5d91ead1148566360c7aec7defa9f6b892902baf
Alexander Korzun <korzun.sas@mail.ru>
2018-10-22 20:35:03 +0300

    It can distinguish between "ноль" and "один" (not ideally though)

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ block_step = 400
 sampling_rate = 8000
 queue_size = 512000
 report_each = 100
-regularization = 0.0
+regularization = 0.0005
 sliding_diff_winsize = 10
 lr_inputs = 20
--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import gzip
+import json
+import hashlib
+import math
 
 import numpy as np
 from argparse import ArgumentParser
@@ -35,9 +40,21 @@ def check_gradient(reg, dataset, weights):
         print(fmt.format('Gradient seems to be incorrect', 'red'))
 
 def main():
-    if '--learn' in sys.argv:
-        learn()
+    if (len(sys.argv) < 2):
+        print('Usage: {} {{collect | learn | recognize}}'.format(sys.argv[0]))
         return
+    command = sys.argv[1]
+    if command == 'learn':
+        learn()
+    elif command == 'collect':
+        collect()
+    elif command == 'recognize':
+        recognize()
+    else:
+        print('Usage: {} {{collect | learn | recognize}}'.format(sys.argv[0]))
+        return
+
+def recognize():
     with AudioRecorder(rate=conf.sampling_rate) as recorder:
         lr = LinearRegression(conf.lr_inputs)
         lr.set_weights([float(i) for i in open('learnt.txt').read().split()])
@@ -51,11 +68,28 @@ def main():
             data = list(recorder.bytes_to_numseq(recorder.finish_recording()))
             transformed_data = list(sliding_diff(data, conf.sliding_diff_winsize))
             scaled_data = scale_array(transformed_data, conf.lr_inputs)
-            print('\x1b[A\x1b[2K' 'Predicted output: {}'.format(lr.get(scaled_data)))
+            raw_pred = lr.get(scaled_data)
+            pred = round(raw_pred)
+            print('\x1b[A\x1b[2K' 'Predicted output: {:.3f} ({})'.format(raw_pred, pred))
 
-def learn():
-    lr = LinearRegression(conf.lr_inputs)
-    dataset = []
+def write_example(data):
+    data_to_write = gzip.compress(json.dumps(data).encode())
+    data_hash = hashlib.sha256(data_to_write).hexdigest()
+    if not os.path.isdir('data/'):
+        os.mkdir('data/')
+    filename = 'data/{}.json.gz'.format(data_hash[:20])
+    with open(filename, 'wb') as f:
+        f.write(data_to_write)
+
+def read_examples():
+    for basename in os.listdir('data/'):
+        fullname = 'data/' + basename
+        with open(fullname, 'rb') as f:
+            encoded = f.read()
+        x, y = json.loads(gzip.decompress(encoded).decode())
+        yield x, y
+
+def collect():
     with AudioRecorder(rate=conf.sampling_rate) as recorder:
         while True:
             print('Type? [1/0/q]: ', end='')
@@ -74,12 +108,21 @@ def learn():
             print('--- FINISHED RECORDING ---')
             transformed_data = list(sliding_diff(data, conf.sliding_diff_winsize))
             scaled_data = scale_array(transformed_data, conf.lr_inputs)
-            dataset.append((scaled_data, y))
+            write_example((scaled_data, y))
+
+def learn():
+    lr = LinearRegression(conf.lr_inputs)
+    dataset = list(read_examples())
     print('Learning...')
     lr.learn(dataset)
     print('Learnt')
     for x, y in dataset:
-        print('y = {}, predicted_y = {}'.format(y, lr.get(x)))
+        pred = lr.get(x)
+        if abs(y - pred) < 0.5:
+            ok = '\x1b[32m(  OK  )\x1b[0m'
+        else:
+            ok = '\x1b[31m( FAIL )\x1b[0m'
+        print('y = {}, predicted_y = {:.3f}   {}'.format(y, pred, ok))
     with open('learnt.txt', 'w') as wf:
         wf.write(' '.join(map(str, lr.weights)))
     print('Weigts written to learnt.txt')
