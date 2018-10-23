a4a06298824bb4387f138d2df47d95562f77bd09
Alexander Korzun <korzun.sas@mail.ru>
2018-10-23 12:36:46 +0300

    Done with multi logistic regression

--- multi_logistic_regression.py
+++ multi_logistic_regression.py
@@ -3,26 +3,35 @@
 from linear_regression import LinearRegression
 
 class MultiLogisticRegression:
-    def __init__(self, labels):
-        self.lrs = {label: LinearRegression for label in labels}
+    def __init__(self, labels, ins):
+        self.lrs = {label: LinearRegression(ins) for label in labels}
 
     def at(self, label):
         return self.lrs[label]
 
     def get(self, x):
-        predicted = {label: self.lrs[label].get(x) for label in labels}
-        predicted_tuples = reversed(sorted([(j, i) for i, j in predicted.items()]))
-        return predicted_tuples[0][0]
+        predicted = {label: self.lrs[label].get(x) for label in self.lrs.keys()}
+        predicted_tuples = list(reversed(sorted([(j, i) for i, j in predicted.items()])))
+        return max(predicted_tuples)
+
+    def getlabel(self, x, treshold=0.5):
+        q, label = self.get(x)
+        if q < treshold:
+            return None
+        return label
 
     def learn(self, multidataset):
         datasets = {label: list(multidataset) for label in self.lrs.keys()}
         for label in self.lrs.keys():
             for i, example in enumerate(datasets[label]):
                 datasets[label][i] = example[0], (1 if example[1] == label else 0)
-        print(datasets)
+
+        for i, (label, dset) in enumerate(datasets.items()):
+            print('Training: {} / {}'.format(i + 1, len(datasets)))
+            self.lrs[label].learn(dset)
 
 # TEST
-mlr = MultiLogisticRegression(['foo', 'bar', 'baz'])
+mlr = MultiLogisticRegression(['foo', 'bar', 'baz'], 2)
 mlr.learn([
     ([4, 7], 'foo'),
     ([2, 4], 'baz'),
@@ -32,3 +41,12 @@ mlr.learn([
     ([2, 2], 'baz'),
     ([7, 3], 'foo'),
 ])
+print(mlr.get([4, 7]))
+print(mlr.get([2, 4]))
+print(mlr.get([8, 1]))
+print(mlr.get([2, 6]))
+
+print(mlr.getlabel([4, 7]))
+print(mlr.getlabel([2, 4]))
+print(mlr.getlabel([8, 1]))
+print(mlr.getlabel([2, 6]))
